a4b7ff8683d3065f3c437e07492f2a924dc14021
Blind-Hawk <romanzimmer99@gmail.com>
2023-10-06 13:55:44 +0200

    V1.3 - Additional Fixes
    -Fixed several UTF-8 encoding issues
    -added correct Jupiter JUnit Api
    -additional issues persist but can be neglected (no proper comments in labor files)

--- labor/de/hska/iwi/ads/search/Search.java
+++ labor/de/hska/iwi/ads/search/Search.java
@@ -12,21 +12,21 @@ public interface Search<E extends Comparable<E>> {
    *   <li> i = left - 1 if key < a[left], or</li>
    *   <li> i = right + 1 if a[right] < key</li>
    * </ol>
-   * @param a  sorted in ascending order given by the Comparable<E> implementation 
-   * @param key 
+   * @param a  sorted in ascending order given by the Comparable<E> implementation
+   * @param key
    * @param left start index for the search, 0 <= left <= right
    * @param right end index for the search, 0 <= left <= right < a.length
-   * 
+   *
    * @throws NullPointerException if a is null
    * @throws ArrayIndexOutOfBoundsException if left or right is not within the index bounds of <code>a</code>
    */
   public int search(E [] a, E key, int left, int right);
-  
+
   /**
    * Searches the lowest index position code>key</code> in the
    * sorted array <code>a</code>, if key is an element of a.
-   * 
-   * See {@link #search(Object[], Object, int, int)} with left = 0 and right = a.length - 1.
+   *
+   * See {@link # search(Object[], Object, int, int)} with left = 0 and right = a.length - 1.
    */
   default int search(E [] a, E key) {
     return search(a, key, 0, a.length - 1);
